0a4cbed8aca322360853b8f7ce5e9547e2753462
wafihfzhn <hafizhuddin96@gmail.com>
2020-06-03 19:29:16 +0800

    Add method post for API

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,25 @@
 class Api::V1::ItemsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     items = Item.all
 
     render json: {data: items, status: :ok}
+  end
+
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: {data: item, status: :create}
+    else
+      render json: {data: item.errors, status: :unprocessable_entity}
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :stock, :price)
   end
 end
